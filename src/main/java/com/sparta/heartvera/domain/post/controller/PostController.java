022642aea4c2b5c3f4806848cfbce715c53b0a2f
wondo8449 <wondo8448@gmail.com>
2024-06-19 18:05:55 +0900

    🎉 add - #1 게시물 생성 기능 추가

---
+++ src/main/java/com/sparta/heartvera/domain/post/controller/PostController.java
@@ -0,0 +1,26 @@
+package com.sparta.heartvera.domain.post.controller;
+
+import com.sparta.heartvera.domain.post.dto.PostRequestDto;
+import com.sparta.heartvera.domain.post.service.PostService;
+import com.sparta.heartvera.security.service.UserDetailsImpl;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/posts")
+public class PostController {
+
+    private final PostService postService;
+
+    @PostMapping("/")
+    public ResponseEntity saveRecipe(@Valid @RequestBody PostRequestDto requestDto, @AuthenticationPrincipal UserDetailsImpl userDetails) {
+        return ResponseEntity.ok().body(postService.savePost(requestDto, userDetails.getUser()));
+    }
+}
---
+++ src/main/java/com/sparta/heartvera/domain/post/service/PostService.java
@@ -0,0 +1,22 @@
+package com.sparta.heartvera.domain.post.service;
+
+import com.sparta.heartvera.domain.post.dto.PostRequestDto;
+import com.sparta.heartvera.domain.post.dto.PostResponseDto;
+import com.sparta.heartvera.domain.post.entity.Post;
+import com.sparta.heartvera.domain.post.repository.PostRepository;
+import com.sparta.heartvera.domain.user.entity.User;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class PostService {
+
+    private final PostRepository postRepository;
+
+    public PostResponseDto savePost(PostRequestDto requestDto, User user) {
+        Post post = postRepository.save(new Post(requestDto, user));
+
+        return new PostResponseDto(post);
+    }
+}
