f6fa2b0c96e52d2cb5b07bee9b6d22f068f8eed6
wondo8449 <wondo8448@gmail.com>
2024-06-19 19:52:57 +0900

    🎉 add - #9 Exception 초기 파일 생성

---
+++ src/main/java/com/sparta/heartvera/common/exception/dto/ExceptionResponse.java
@@ -0,0 +1,13 @@
+package com.sparta.heartvera.common.exception.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+@Builder
+@AllArgsConstructor
+@Getter
+public class ExceptionResponse {
+    String msg;
+    String path;
+}
