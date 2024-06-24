41c98c3d329d2d2bfa2bf391d1eb7ae2885e370e
haejungHan <tinley705@gmail.com>
2024-06-24 20:45:57 +0900

    ♻️ refactor - 팔로우한 사람 게시물 조회 수정

--- src/main/java/com/sparta/heartvera/domain/post/repository/PublicPostRepository.java
+++ src/main/java/com/sparta/heartvera/domain/post/repository/PublicPostRepository.java
@@ -2,6 +2,7 @@ package com.sparta.heartvera.domain.post.repository;
 
 import com.sparta.heartvera.domain.post.entity.PublicPost;
 import com.sparta.heartvera.domain.user.entity.User;
+import java.util.Collection;
 import java.util.List;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -10,8 +11,6 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface PublicPostRepository extends JpaRepository<PublicPost, Long> {
-  Page<PublicPost> findByUserOrderByCreatedAtDesc(User user, Pageable pageable);
-
-//  Page<PublicPost> findAllByUser(User followedUser, Pageable pageable);
+  Page<PublicPost> findByUser_UserSeqInOrderByCreatedAtDesc(Collection<Long> userIds, Pageable pageable);
 
 }
